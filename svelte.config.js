683e5e28a29c7177e6363fe01553fffe48646050
fmoctavioe-ux <fmoctavioe@unincca.edu.coi>
2026-03-31 11:13:51 -0500

    Configurar adaptador estático de SvelteKit para GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: 'index.html',
+            precompress: false,
+            strict: false
+        }),
+        paths: {
+            base: '/actividad-2' // Esto le ayuda a GitHub Pages a encontrar tus archivos
+        }
+    }
 };
 
 export default config;
